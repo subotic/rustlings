0270409418a19b24cdf65acc0a67e91374414bfd
Ivan Subotic <400790+subotic@users.noreply.github.com>
2024-04-28 20:57:33 +0200

    add progress

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let cost: i32 = if quantity <= 40 { 2 } else { 1 };
+    return quantity * cost;
+}
 
 // Don't modify this function!
 #[test]
